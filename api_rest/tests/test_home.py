91fbeb7d9a0f484f81ddcf7f1814972e69618803
Luiz Fernando Rojas <82002466+LuizRojas@users.noreply.github.com>
2025-07-09 18:49:12 -0300

    Delete api_rest directory

--- api_rest/tests/test_home.py
+++
@@ -1,6 +0,0 @@
-from app import app
-
-def test_home_status_code():
-    with app.test_client() as client:
-        response = client.get('/')
-        assert response.status_code in (200, 404)  # api disponivel?
